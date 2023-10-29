75e3cfb2b6fb06032e2b4c012d67cd9d05bd3f7b
Usama Khan <usamaoff796@gmail.com>
2023-10-30 01:07:15 +0500

    Version Updated to 1.0.1

--- amadeus-plugin.php
+++ amadeus-plugin.php
@@ -5,13 +5,13 @@
  * Description: This plugin is use to show cities autocompletes on simple text-fields of any form of the website.
  * Author: Usama Khan
  * Author URI: https://github.com/usamakhan143
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires PHP: 7.4
  * Text Domain: amadeus-plugin-translate
  */
 
 
-if( !defined('ABSPATH') ) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
@@ -19,7 +19,8 @@ if( !defined('ABSPATH') ) {
 if (!class_exists('AmadeusPlugin')) {
 
 
-    class AmadeusPlugin {
+    class AmadeusPlugin
+    {
 
         function __construct()
         {
@@ -34,10 +35,7 @@ if (!class_exists('AmadeusPlugin')) {
             include_once(AMADEUS_PLUGIN_PATH . 'includes/amadeus-plugin.php');
             include_once(AMADEUS_PLUGIN_PATH . 'includes/options-page.php');
         }
-
     }
-
-
 }
 
 
@@ -45,10 +43,11 @@ $AmadeusPlugin = new AmadeusPlugin();
 $AmadeusPlugin->initialize();
 
 
-add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'salcode_add_plugin_page_settings_link');
-function salcode_add_plugin_page_settings_link( $links ) {
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'ap_add_plugin_page_settings_link');
+function ap_add_plugin_page_settings_link($links)
+{
     $links[] = '<a href="' .
-        admin_url( 'tools.php?page=crb_carbon_fields_container_amadeus.php' ) .
+        admin_url('tools.php?page=crb_carbon_fields_container_amadeus.php') .
         '">' . __('Settings') . '</a>';
     return $links;
 }
