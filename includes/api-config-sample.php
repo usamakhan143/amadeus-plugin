f6d2995a9866bfdac12afa1638574148acde985b
Usama Khan <usamaoff796@gmail.com>
2023-11-01 18:28:43 +0500

    Update API config file.

--- includes/api-config-sample.php
+++ includes/api-config-sample.php
@@ -3,6 +3,7 @@
 $LIVE_BASE_URL = "";
 $TEST_BASE_URL = ""; // https://abc.com/amadeus/v1
 $TOKEN_API = "/security/oauth2/token"; // Endpoint: /token
+$AIRPORT_API = "/reference-data/locations?subType=AIRPORT&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=analytics.travelers.score&view=FULL&";
 $CITIES_API = "/reference-data/locations/cities?"; // Endpoint: /cities
 $CLIENT_ID = carbon_get_theme_option('client_id'); // XXXXXXXXXXXXXXXXXXXXX
 $CLIENT_SECRET = carbon_get_theme_option('client_secret'); // XXXXXXXXXXXXXXXXX
